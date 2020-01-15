6bbf670b76894dfdc829fe042d67ce52648c84de
fabianoramos78 <contato@fabianoramos.dev>
2020-01-15 20:45:23 -0300

    Refactor

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose')
+const cors = require('cors')
 const routes = require('./routes')
 
 const app = express();
@@ -10,16 +11,10 @@ mongoose.connect('mongodb+srv://fabiano:141501@cluster0-x0gzq.mongodb.net/ominst
     useCreateIndex: true
 })
 
-app.use(express.json(), routes) // permitindo JSON na aplicação com Express
-//app.use(routes) // vinculando as rotas no index.js
-
-/***
- * Métodos HTTP => GET, POST, PUT & DELETE
- * 
- * Tipos de Parâmetros:
- * Query Params: request.query (Filtros, Ordenação, Paginação => queries embutidas na URL de requisição)
- * Route Params: request.params (Identificar um recurso na alteração/remoção)
- * Body: request.body (dados para criação aou alteração de um registro => POST/PUT)
- */
+app.use(
+    cors(),
+    express.json(), 
+    routes
+)
 
 app.listen(3333);
